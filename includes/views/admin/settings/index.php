a95bbf4d4fae83c46d87215cfaf6ee51c197efa4
Nazmul Hasan Robin <robin.sust08@gmail.com>
2025-01-12 21:15:37 +0600

    Username for dashboard page only

--- includes/views/admin/settings/index.php
+++ includes/views/admin/settings/index.php
@@ -6,16 +6,17 @@ global $wp;
                     
 $current_url = is_admin() ? admin_url("tools.php?page={$this->page_slug}") : home_url(add_query_arg(array(), $wp->request));
 $is_shortcode = ! is_admin() ? 1 : 0;
+$hide_on_shortcode = $is_shortcode ? 'hidden' : '';
 
 ?>
 
 <div class="wrap p-6 max-w-4xl mx-auto">
-    <div class="bg-white shadow rounded-lg p-6 mb-6 hidden">
+    <div class="bg-white shadow rounded-lg p-6 mb-6 <?php echo esc_attr( $hide_on_shortcode ); ?>">
         <h2 class="text-2xl font-semibold text-gray-800">
             <?php echo esc_html__('WordPress Core Contributions', 'nhrrob-core-contributions'); ?>
         </h2>
 
-        <form method="post" action="<?php echo esc_url( $current_url ); ?>" class="space-y-4 mt-4 hidden">
+        <form method="post" action="<?php echo esc_url( $current_url ); ?>" class="space-y-4 mt-4 <?php echo esc_attr( $hide_on_shortcode ); ?>">
             <div>
                 <?php wp_nonce_field('nhrcc_form_action', 'nhrcc_form_nonce'); ?>
                 <input type="text" id="nhrcc_username" name="nhrcc_username" value="<?php echo esc_attr($username); ?>" class="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" required>
